c6087eee5c67c6b2c97b306d87141da59df4f608
Flandre Scarlet <the.best.vampire.in.gensokyo@gmail.com>
2024-09-18 03:13:34 +0700

    Сделал проверку 1

--- src/com/lesson/projects/passwordGenerator/Main.java
+++ src/com/lesson/projects/passwordGenerator/Main.java
@@ -1,8 +1,21 @@
 package com.lesson.projects.passwordGenerator;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        FirstLevelGen passLvlOne
         System.out.println("Введите размер пароля. Не более 30 символов");
-
+        if (checkPass()){
+            System.out.println("");
+        }
+    }
+    static boolean checkPass (){
+        Scanner scan = new Scanner(System.in);
+        int temp = scan.nextInt();
+        if (temp>30) {
+            return true;
+        }
+        return false;
     }
 }
