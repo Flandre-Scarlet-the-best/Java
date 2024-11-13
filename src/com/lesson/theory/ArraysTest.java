5cc74a92d8acee530cb91f70d4ee3da4ea2926b9
Flandre Scarlet <the.best.vampire.in.gensokyo@gmail.com>
2024-11-13 16:21:15 +0700

    обновление 2

--- src/com/lesson/theory/ArraysTest.java
+++ src/com/lesson/theory/ArraysTest.java
@@ -1,22 +1,22 @@
-package com.lesson.theory;
-
-import java.util.Arrays;
-import java.util.Random;
-
-
-public class ArraysTest {
-    public static void main(String[] args) {
-     int [] array = new int[20];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = new Random().nextInt(100);
-        }
-        System.out.println(Arrays.toString(array));
-        for (int i = 0; i < array.length/2;i++) {
-            int temp = array[i];
-            array[i] = array[array.length - (1+i)];
-            array[array.length - (1+i)] = temp;
-        }
-
-        System.out.println(Arrays.toString(array));
-    }
-}
+//package com.lesson.theory;
+//
+//import java.util.Arrays;
+//import java.util.Random;
+//
+//
+//public class ArraysTest {
+//    public static void main(String[] args) {
+//     int [] array = new int[20];
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = new Random().nextInt(100);
+//        }
+//        System.out.println(Arrays.toString(array));
+//        for (int i = 0; i < array.length/2;i++) {
+//            int temp = array[i];
+//            array[i] = array[array.length - (1+i)];
+//            array[array.length - (1+i)] = temp;
+//        }
+//
+//        System.out.println(Arrays.toString(array));
+//    }
+//}
